ab82da4cb59ed5d77e62fa87d202e54ba929eb34
Rahul Dhangar <rahuldhangar@gmail.com>
2019-10-18 16:21:55 +0530

    example usage of core modules: path, util, v8

---
+++ NodeJS/Node.js Essential Training/core.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+// path module is a core module available by default with node.js
+const path = require("path");
+// load another core module: utility
+const util = require("util");
+//load v8 module
+const v8 = require("v8");
+
+// example of core path module to create a directory uploads path
+const dirUploads = path.join(__dirname, "www", "files", "uploads");
+console.log(dirUploads);
+
+// use log() function of util module which is another powerful logging function
+util.log( path.basename(__filename) );
+util.log(" ^ The name of the current file");
+
+// take a look at memory usage, etc using v8 module
+util.log(v8.getHeapStatistics());
