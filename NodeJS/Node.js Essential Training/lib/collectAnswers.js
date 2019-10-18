40c76a343d92bc84b14945b65519362f46e98d7e
Rahul Dhangar <rahuldhangar@gmail.com>
2019-10-18 19:35:21 +0530

    Improve a module with EventEmitter

--- NodeJS/Node.js Essential Training/lib/collectAnswers.js
+++ NodeJS/Node.js Essential Training/lib/collectAnswers.js
@@ -2,6 +2,9 @@
 // include the readline module using require function
 const readLine = require("readline");
 
+const { EventEmitter } = require("events");
+const emitter = new EventEmitter();
+
 const rl = readLine.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -14,13 +17,17 @@ module.exports = (questions, done = f => f) => {
 
     // function to be invoked upon answering the question...
     const questionAnswered = answer => {
+        emitter.emit("answer", answer);
         answers.push(answer);
         if (answers.length < questions.length) {
             rl.question(questions[answers.length], questionAnswered);
         } else {
+            emitter.emit("complete", answers);
             done(answers);
         }
     };
     
     rl.question(firstQuestion, questionAnswered);
+
+    return emitter;
 };
