d17ff4cf9829142fe34ea7b06d62e3e5c2dff422
Rahul Dhangar <rahuldhangar@gmail.com>
2019-10-18 16:09:05 +0530

    report progress in percentage with setInterval

--- NodeJS/Node.js Essential Training/timers.js
+++ NodeJS/Node.js Essential Training/timers.js
@@ -1,17 +1,29 @@
-const waitTime = 3000;
+const waitTime = 5000;
 const waitInterval = 500;
 let currentTime = 0;
 
 //
 const incTime = () => {
     currentTime += waitInterval;
-    console.log(`waiting ${currentTime / 1000} seconds`);
+    // create a var to calculate percentage for progress
+    const p = Math.floor((currentTime / waitTime) * 100);
+    // clear the last line...
+    process.stdout.clearLine();
+    // move cursor back to start position...
+    process.stdout.cursorTo(0);
+    // write our message using process
+    process.stdout.write(`waiting ... ${p}% `);
 };
 
 console.log(`setting a ${waitTime / 1000} second delay`);
 
+// function to clear the interval and display finished message
 const timerFinished = () => {
     clearInterval(interval);
+    // clear the last line and move cursor back to start position...
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    console.log("done");
 };
 
 //setInterval function actually returns the interval itself so that we can clear it later
