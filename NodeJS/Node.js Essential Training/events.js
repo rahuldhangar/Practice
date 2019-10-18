c0fa3dbe798742799456326594c900cc29bc79dd
Rahul Dhangar <rahuldhangar@gmail.com>
2019-10-18 19:02:41 +0530

    Raise and handle custom events with EventEmitter

---
+++ NodeJS/Node.js Essential Training/events.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+
+/**
+ * Events Emitter is Node.js's implementation of Pub/Sub design pattern and
+ * it gives a mechanism for emitting custom events and wiring up listeners and heandlers for those events
+ */
+
+// get events module (no need to put the file path because its a core module)
+const events = require("events");
+
+// create instance of the eventsEmitter
+const emitter = new events.EventEmitter();
+
+// emitter.on() function to handle these custom events
+emitter.on("customEvent", (message, user) => {
+    console.log(`${user}: ${message}`);
+});
+
+// Raise two custom events... by emitter.emit() function
+emitter.emit("customEvent", "Hello World", "Computer");
+emitter.emit("customEvent", "That's pretty cool huh?", "Rahul");
