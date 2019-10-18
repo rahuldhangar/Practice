064d96cdb74c618fc5fd3daf07d65b6ef946a288
Rahul Dhangar <rahuldhangar@gmail.com>
2019-10-18 18:48:00 +0530

    Rework on questions_2 and separate the collectAnswers function in a separate module for reusability

---
+++ NodeJS/Node.js Essential Training/lib/collectAnswers.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+// include the readline module using require function
+const readLine = require("readline");
+
+const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+module.exports = (questions, done) => {
+    const answers = [];
+    // destructuring the question from the array of questions
+    const [firstQuestion] = questions;
+
+    // function to be invoked upon answering the question...
+    const questionAnswered = answer => {
+        answers.push(answer);
+        if (answers.length < questions.length) {
+            rl.question(questions[answers.length], questionAnswered);
+        } else {
+            done(answers);
+        }
+    };
+    
+    rl.question(firstQuestion, questionAnswered);
+};
