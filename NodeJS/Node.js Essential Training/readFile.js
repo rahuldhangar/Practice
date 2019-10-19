54be9f377d8f9c4657be2f24f79e5945d90dc17a
Rahul Dhangar <rahuldhangar@gmail.com>
2019-10-20 00:24:22 +0530

    Example of reading a file asynchronously

--- NodeJS/Node.js Essential Training/readFile.js
+++ NodeJS/Node.js Essential Training/readFile.js
@@ -3,7 +3,11 @@
  */
 const fs = require("fs");
 
-// This function below will read file synchronously
-const text = fs.readFileSync("./assets/Readme.md", "UTF-8");
+// // This function below will read file synchronously
+// const text = fs.readFileSync("./assets/Readme.md", "UTF-8");
 
-console.log(text);
+// This function below will read file asynchronously
+fs.readFile("./assets/Readme.md", "UTF-8", (err, text) => {
+    console.log("file contents read");
+    console.log(text);
+});
