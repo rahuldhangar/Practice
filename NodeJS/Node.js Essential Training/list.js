4c389001b33252f7575ae6bc2d9e7242a1471a66
Rahul Dhangar <rahuldhangar@gmail.com>
2019-10-20 00:17:49 +0530

    Example of reading same files in directory asynchronously

--- NodeJS/Node.js Essential Training/list.js
+++ NodeJS/Node.js Essential Training/list.js
@@ -3,14 +3,30 @@
 //load the fs module using require() function
 const fs = require("fs");
 
-console.log("started reading files");
-//function to read all files in a directory synchronously (blocking code)
-const files = fs.readdirSync("./assets");
-console.log("complete");
+// console.log("started reading files");
+// //function to read all files in a directory synchronously (blocking code)
+// const files = fs.readdirSync("./assets");
+// console.log("complete");
 
-console.log(files);
+// console.log(files);
 
 /**
- * Notice that the line #8 is blocking the code because the files are read synchronously this way.
- * In the next commit, we will see how can we read files asynchronously.
+ * Above code from line #6 to #11 an example of reading files synchronously which blocks the code.
+ * Below mentioned code reads the files asynchronously.
 */
+
+// using readdir() function in place of readdirSync() for asynchronously reading the directory content
+fs.readdir("./assets", (err, files) => {
+    if( err ) {
+        throw err;
+    }
+    console.log("complete");
+    console.log(files);
+});
+
+console.log("started reading files");
+
+/**
+ * Notice that the line 27 still gets displayed on console first because message on line 23
+ * only gets displayed once the files in the directory are read. So this is being done asynchronously
+ */
