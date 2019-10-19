8cc5917e07feede1037c8881d475633a16cfa2d2
Rahul Dhangar <rahuldhangar@gmail.com>
2019-10-20 00:30:14 +0530

    Example of reading a text file syncronously & asynchronously and a binary file (image) asynchronously

--- NodeJS/Node.js Essential Training/readFile.js
+++ NodeJS/Node.js Essential Training/readFile.js
@@ -6,8 +6,17 @@ const fs = require("fs");
 // // This function below will read file synchronously
 // const text = fs.readFileSync("./assets/Readme.md", "UTF-8");
 
-// This function below will read file asynchronously
-fs.readFile("./assets/Readme.md", "UTF-8", (err, text) => {
-    console.log("file contents read");
-    console.log(text);
+// // This function below will read file asynchronously
+// fs.readFile("./assets/Readme.md", "UTF-8", (err, text) => {
+//     console.log("file contents read");
+//     console.log(text);
+// });
+
+// This function below will read a binary file asynchronously in the buffer
+fs.readFile("./assets/alex.jpg", (err, img) => {
+    if( err ) {
+        console.log(`An error has occured: ${err.message}`);
+    }
+    console.log("binary file contents read");
+    console.log(img);
 });
